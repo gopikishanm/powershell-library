fd08fbc30cf0be208985b1ad902681261b087c30
Naren der <gopikishan.mariyala@gmail.com>
2018-05-15 20:14:16 +1000

    Added SearchFile Script

---
+++ SearchFile.ps1
@@ -0,0 +1,23 @@
+# This command let is for searching a given directory for specific file type
+# The results provides the file name, along with last modified date and size
+Function Search-Dir {
+    [cmdletbinding()]
+    Param(
+    [string]$directoryPath,
+    [string]$fileType
+    )
+    Process{
+       #Get-ChildItem -Path $directoryPath -Filter $fileType -Recurse | % { Write-Host $_.FullName}
+       $printArray=[System.Collections.ArrayList]@()
+       Get-ChildItem -Path $directoryPath -Filter $fileType -Recurse | % { 
+       $item = New-Object -TypeName PSObject
+       $item | Add-Member -Name FileName -Value $_.Name -MemberType NoteProperty
+       $item | Add-Member -Name Directory -Value $_.Directory -MemberType NoteProperty
+       $item | Add-Member -Name LastModified -Value $_.LastAccessTime -MemberType NoteProperty
+       $item | Add-Member -Name Size -Value $_.Length -MemberType NoteProperty
+       #Write-Host $item
+       $printArray+=$item
+      }
+      Write-Host $printArray.ForEach({[PSCustomObject]$_}) | Format-List
+    }
+}
